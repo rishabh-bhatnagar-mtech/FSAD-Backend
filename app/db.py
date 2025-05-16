32fb1b6dcacae322350b377dab81d6e746ed7b78
Rishabh Bhatnagar <2023tm93760@wilp.bits-pilani.ac.in>
2025-05-16 11:57:33 +0000

    Update host of db in the backend service
    Signed-off-by: Rishabh Bhatnagar <2023tm93760@wilp.bits-pilani.ac.in>

--- app/db.py
+++ app/db.py
@@ -3,6 +3,5 @@ import psycopg
 
 def get_db():
     return psycopg.connect(
-
-        dbname="fsad", user="postgres", password="admin", host="localhost", port=5432
+        dbname="fsad", user="postgres", password="admin", host="fsad_db", port=5432
     )
